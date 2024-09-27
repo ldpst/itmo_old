b32460f74cee18f1b24015485c4fac6b5b7febcc
ldpst <89788168+ldpst@users.noreply.github.com>
2024-09-27 23:18:55 +0300

    Add files via upload

---
+++ labs/sem1/opd/lab1/p2.sh
@@ -0,0 +1,32 @@
+cd lab0
+chmod 624 beheeyem4
+chmod 335 metagross6
+cd metagross6
+chmod u+rx-w ninjask
+chmod g+rwx ninjask
+chmod o+rwx ninjask
+chmod 500 nosepass
+chmod 046 wooper
+chmod 066 kricketune
+chmod u-rwx armaldo
+chmod g-rwx armaldo
+chmod u+rw-x armaldo
+cd -
+chmod 660 noctowl6
+chmod 737 pansage6
+cd pansage6
+chmod 666 xatu
+chmod 737 haxorus
+chmod 512 purrloin
+chmod 755 aipom
+chmod 752 lilligant
+cd -
+chmod u+rwx shelgon9
+chmod g-r+wx shelgon9
+chmod o+rwx shelgon9
+cd shelgon9
+chmod 551 charizard
+chmod 500 hoothoot
+chmod 044 munchlax
+cd -
+chmod 004 togetic0
