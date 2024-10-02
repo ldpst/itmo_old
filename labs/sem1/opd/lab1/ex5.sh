fe5cf54ddfaa8d1e510e4439f9e05c27bae1ac10
ldpst <89788168+ldpst@users.noreply.github.com>
2024-10-02 18:07:25 +0300

    Add files via upload

---
+++ labs/sem1/opd/lab1/ex5.sh
@@ -0,0 +1,12 @@
+cd lab0
+rm -f beheeyem4
+chmod 777 metagross6
+cd metagross6
+rm -f kricketune
+rm kricketunenocto*
+rm woopernocto*
+cd ..
+chmod 335 metagross6
+rm -rf pansage6
+rm -rf pansage6/lilligant
+cd ..
