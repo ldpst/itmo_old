6915cbd80d2d1752d4956f6e0fcc6e6977204b9c
ldpst <89788168+ldpst@users.noreply.github.com>
2024-10-12 00:37:00 +0300

    Delete labs/sem1/prog/lab2/pokeLab directory

--- labs/sem1/prog/lab2/pokeLab/src/PokeLab.java
+++
@@ -1,8 +0,0 @@
-import pokemons.Pichu;
-
-public class PokeLab {
-    public static void main(String[] args) {
-        Pichu test1 = new Pichu("test1", 1);
-        System.out.println(test1);
-    }
-}
