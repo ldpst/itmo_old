f67dc209463ac61fed0535762bea086fbbb33057
ldpst <89788168+ldpst@users.noreply.github.com>
2024-10-18 19:56:22 +0300

    Update task1.py

--- labs/sem1/inf/lab3/task1.py
+++ labs/sem1/inf/lab3/task1.py
@@ -7,31 +7,36 @@ def task(s):
 
 def test1():
     print('\n=================test1=================')
-    print("==-{P-=-{P{=-{P{{=-{P=-{P{=-{P, 6")
+    print("==-{P-=-{P{=-{P{{=-{P=-{P{=-{P")
+    print("ans = 6")
     print("res =", task("==-{P-=-{P{=-{P{{=-{P=-{P{=-{P"))
 
 
 def test2():
     print('\n=================test2=================')
-    print("= - { P=-{P= - { P, 1")
+    print("= - { P=-{P= - { P")
+    print("ans = 1")
     print("res =", task("= - { P=-{P= - { P"))
 
 
 def test3():
     print('\n=================test3=================')
-    print("======------{{{{{{PPPPPP, 0")
+    print("======------{{{{{{PPPPPP")
+    print("ans = 0")
     print("res =", task("======------{{{{{{PPPPPP"))
 
 
 def test4():
     print('\n=================test4=================')
-    print("=-\\{P=-\\{P=-\\{P=-{P=-\\{P=-\\{P, 1")
+    print("=-\\{P=-\\{P=-\\{P=-{P=-\\{P=-\\{P")
+    print("ans = 1")
     print("res =", task("=-\\{P=-\\{P=-\\{P=-{P=-\\{P=-\\{P"))
 
 
 def test5():
     print('\n=================test5=================')
-    print("testingtesting=-{Ptesting=-testing{P, 1")
+    print("testingtesting=-{Ptesting=-testing{P")
+    print("ans = 1")
     print("res =", task("testing testing=-{Ptesting=-testing{P"))
 
 
