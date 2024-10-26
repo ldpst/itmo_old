1fd5c5e073b8e6eacf83a571db503dae5938d284
ldpst <89788168+ldpst@users.noreply.github.com>
2024-10-27 01:24:00 +0300

    Update main.py

--- labs/sem1/inf/lab4/additional1/main.py
+++ labs/sem1/inf/lab4/additional1/main.py
@@ -1,1 +1,12 @@
+from json import loads
+from os import path
+from dicttoxml import dicttoxml
 
+def convert_to_xml(string: str):
+    json_data = loads(string)
+    xml = dicttoxml(json_data).decode()
+    return xml
+
+if __name__ == "__main__":
+    json = open(path.join(path.dirname(__file__), "../in.json"), 'r').read()
+    open(path.join(path.dirname(__file__), "../out-add1.xml"), 'w').write(convert_to_xml(json))
