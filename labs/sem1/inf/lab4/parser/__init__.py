e02da17f3b1db10c6097ebdebabd66d68a9e4aaf
ldpst <89788168+ldpst@users.noreply.github.com>
2024-10-27 00:44:09 +0300

    Add files via upload

--- labs/sem1/inf/lab4/parser/__init__.py
+++ labs/sem1/inf/lab4/parser/__init__.py
@@ -1,77 +1,97 @@
-from functools import reduce
+from parser.converter import convert_to_xml
 
 
 def parse_json(json: str):
-    xml = ""
-    # print(json)
-    print(__parse_value(json.strip()))
+    string = parse_value(json.strip())[0]
+    return string
 
 
-def __parse_keyvalue(string: str):
-    parsed_string = __parse_string(string)
+
+def parse_keyvalue(string: str):
+    parsed_string = parse_string(string)
     if parsed_string is None:
         return None
-    parsed_colon = __parse_colon(parsed_string[1])
+    parsed_colon = parse_colon(parsed_string[1])
     if parsed_colon is None:
         return None
-    parsed_value = __parse_value(parsed_colon[1])
+    parsed_value = parse_value(parsed_colon[1])
     if parsed_value is None:
         return None
     return (parsed_string[0], parsed_value[0]), parsed_value[1].strip()
 
 
-def __parse_value(string: str):
-    res = reduce(
-        lambda f, g: f if f(string) else g,
-        [
-            __parse_number,
-            __parse_string,
-            __parse_array,
-            __parse_object,
-        ],
-    )(string)
-    if res is None:
-        return None
-    else:
-        return res[0], res[1].strip()
+def parse_value(string: str):
+    # res = ("", string)
+    parsed_number = parse_number(string)
+    if not parsed_number is None:
+        parse_value(parsed_number[1].strip())
+        return parsed_number[0], parsed_number[1].strip()
+    parsed_string = parse_string(string)
+    if not parsed_string is None:
+        parse_value(parsed_string[1].strip())
+        return parsed_string[0], parsed_string[1].strip()
+    parsed_array = parse_array(string)
+    if not parsed_array is None:
+        parse_value(parsed_array[1].strip())
+        return parsed_array[0], parsed_array[1].strip()
+    parsed_obect = parse_object(string)
+    if not parsed_obect is None:
+        parse_value(parsed_obect[1].strip())
+        return parsed_obect[0], parsed_obect[1].strip()
+
+    # res = reduce(
+    #     lambda f, g: f if f(string) else g,
+    #     [
+    #         parse_number,
+    #         parse_string,
+    #         parse_array,
+    #         parse_object,
+    #     ],
+    # )(string)
+    # print(res)
+    # print("\n\n\n")
+    # if res is None:
+    #     return None
+    # else:
+    #     return res[0], res[1].strip()
 
 
-def __parse_comma(string: str):
+def parse_comma(string: str):
     if not string.startswith(','):
         return None
     return ",", string[1:].strip()
 
 
-def __parse_comma_values(string: str):
+def parse_comma_values(string: str):
     res = []
     while True:
-        parsed_value = __parse_value(string)
+        parsed_value = parse_value(string)
         if parsed_value is None:
             break
         res.append(parsed_value[0])
         string = parsed_value[1]
 
-        parsed_comma = __parse_comma(string)
+        parsed_comma = parse_comma(string)
         if parsed_comma is None:
             break
         string = parsed_comma[1]
     return res, string.strip()
 
 
-def __parse_array(string: str):
+def parse_array(string: str):
     if not string.startswith('['):
         return None
-    parse_comma_values = __parse_comma_values(string[1:].strip())
-    if parse_comma_values is None:
+    parsed_comma_values = parse_comma_values(string[1:].strip())
+    if parsed_comma_values is None:
         res, string = [], string[1:]
     else:
-        res, string = parse_comma_values
+        res, string = parsed_comma_values
     if not string.startswith(']'):
         return None
     return res, string[1:].strip()
 
 
-def __parse_string(string: str):
+def parse_string(string: str):
     if not string.startswith("\""):
         return None
     second_quote = string.find("\"", 1)
@@ -80,40 +100,40 @@ def __parse_string(string: str):
     return string[1:second_quote], string[second_quote + 1:]
 
 
-def __parse_colon(string: str):
+def parse_colon(string: str):
     if not string.startswith(":"):
         return None
     else:
         return ":", string[2:].strip()
 
 
-def __parse_comma_keyvalues(string: str):
+def parse_comma_keyvalues(string: str):
     res = {}
     while True:
-        parsed_keyvalue = __parse_keyvalue(string)
+        parsed_keyvalue = parse_keyvalue(string)
         if parsed_keyvalue is None:
             break
         key, val = parsed_keyvalue[0]
         res[key] = val
         string = parsed_keyvalue[1]
 
-        parsed_comma = __parse_comma(string)
+        parsed_comma = parse_comma(string)
         if parsed_comma is None:
             break
         string = parsed_comma[1].strip()
     return res, string.strip()
 
 
-def __parse_object(string: str):
+def parse_object(string: str):
     if not string.startswith("{"):
         return None
-    arr, string = __parse_comma_keyvalues(string[1:].strip())
+    arr, string = parse_comma_keyvalues(string[1:].strip())
     if not string.startswith("}"):
         return None
     return arr, string[1:].strip()
 
 
-def __parse_number(string: str):
+def parse_number(string: str):
     num = ""
     for i in string:
         if i in "0123456789.Ee-+":
