ca320c523b092aa64a3b372436df9c31083ee4b3
ldpst <89788168+ldpst@users.noreply.github.com>
2024-10-27 00:45:53 +0300

    Add files via upload

--- labs/sem1/inf/lab4/main.py
+++ labs/sem1/inf/lab4/main.py
@@ -1,3 +1,5 @@
 from parser import parse_json
-f = open("in.json", 'r')
-parse_json(f.read())
+from parser.converter import convert_to_xml
+json = open("in.json", 'r')
+xml = open("out.xml", 'w')
+xml.write(convert_to_xml(parse_json(json.read())))
