52da090b34a755e83b5d143bee3ef45315962829
ldpst <89788168+ldpst@users.noreply.github.com>
2024-10-27 03:50:00 +0300

    Delete labs/sem1/inf/lab4 directory

--- labs/sem1/inf/lab4/main_task/main.py
+++
@@ -1,7 +0,0 @@
-from main_task.parser import parse_json
-from main_task.parser.converter import convert_to_xml
-from os import path
-
-json = open(path.join(path.dirname(__file__), "../data/in.json"), 'r').read()
-xml = open(path.join(path.dirname(__file__), "../data/out.xml"), 'w')
-xml.write(convert_to_xml(parse_json(json)))
