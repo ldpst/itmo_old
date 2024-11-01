de54bc90f5ce8020db099b1741fee4209a1eb7c4
Fedor Melnik <89788168+ldpst@users.noreply.github.com>
2024-11-01 22:56:30 +0300

    Delete labs/sem1/inf/lab4/tasks directory

--- labs/sem1/inf/lab4/tasks/additional5/main.py
+++
@@ -1,7 +0,0 @@
-from parser import parse_json
-from parser.converter import convert_to_csv
-from os import path
-
-json = open(path.join(path.dirname(__file__), "../data/in.json"), 'r').read()
-csv = open(path.join(path.dirname(__file__), "../data/out-add5.csv"), 'w')
-csv.write(convert_to_csv(parse_json(json)))
